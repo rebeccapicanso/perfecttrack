87505260dcc736d0d1d68c264982f5672feaa642
Rebecca Picanso <76741837+rebeccapicanso@users.noreply.github.com>
2024-03-01 08:39:18 -0500

    Create shot_labeling.py

---
+++ shot_labeling.py
@@ -0,0 +1,2 @@
+# iterates through each shot, identifying objects. 
+# Set up with two different libraries - one that will provide a probabilty indexing, another that will identify predetermined shots.
