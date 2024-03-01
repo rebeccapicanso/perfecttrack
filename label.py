42c50086b84809430d43be3475db9e443f56bc78
Rebecca Picanso <76741837+rebeccapicanso@users.noreply.github.com>
2024-03-01 08:47:40 -0500

    Update label.py

--- label.py
+++ label.py
@@ -1,2 +1,71 @@
-# iterates through each shot, identifying objects. 
-# Set up with two different libraries - one that will provide a probabilty indexing, another that will identify predetermined shots.
+# iterates through each shot, identifying objects in that scene and indexing in a .json.
+
+import json
+import os
+
+from moviepy.editor import VideoFileClip
+from PIL import Image
+from transformers import pipeline
+
+from shots import get_shots
+
+
+MODEL = "google/vit-base-patch16-224"
+
+# truly any model will work here - it's recommended to keep the model small
+# unless you're using colab or a machine with a lot of memory.
+labeler = pipeline("image-classification", model=MODEL)
+
+def label_scenes(videofile, scenes):
+    # file name to save the labels to
+    outname = videofile + ".labels.json"
+
+    # if the file already exists, don't re-label, just load the file
+    if os.path.exists(outname):
+        with open(outname, "r") as infile:
+            return json.load(infile)
+
+    clip = VideoFileClip(videofile)
+
+    out = []
+
+    # go through every scene in the video
+    for i, scene in enumerate(scenes):
+        start_time = scene["start"]
+        end_time = scene["end"]
+
+        # extract a the first frame of the scene
+        frame = clip.get_frame(start_time)
+
+        # convert the frame into an image
+        img = Image.fromarray(frame)
+
+        # get a label from the image
+        results = labeler(img)
+
+        print("scene", i, start_time)
+        for r in results:
+            print(r["score"], r["label"])
+
+        item = {
+            "start": start_time,
+            "end": end_time,
+            "content": results[0]["label"],
+            "labels": results,
+        }
+
+        out.append(item)
+
+    # save all the labels
+    with open(outname, "w") as outfile:
+        json.dump(out, outfile, indent=2)
+
+    # return the list of labeled scenes
+    return out
+
+if __name__ == "__main__":
+    import sys
+
+    for f in sys.argv[1:]:
+        scenes = get_shots(f)
+        label_scenes(f, scenes)
